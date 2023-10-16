66fd0566352a20ce3421a2b16c87f5e146964a42
Ville Korhonen <ville11_87@hotmail.com>
2023-10-16 13:32:19 +0300

    Tehtävä valmis

--- App.js
+++ App.js
@@ -1,11 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { useState } from 'react';
+import { View, StyleSheet, TouchableOpacity, TextInput, Text, Keyboard } from 'react-native';
+import * as Speech from 'expo-speech';
 
 export default function App() {
+  const [input, setInput] = useState('');
+
+  const speak = () => {
+    Speech.speak(input);
+    Keyboard.dismiss();
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.header}>Speaker app</Text>
+      <TextInput style={styles.input}
+        placeholder='Write text here'
+        onChangeText={text => setInput(text)}
+        value={input}>
+      </TextInput>
+
+      <TouchableOpacity style={styles.button} title="button" onPress={speak}>
+        <Text style={styles.text}> Press to hear</Text>
+         </TouchableOpacity>
     </View>
   );
 }
@@ -13,8 +30,39 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#262626',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  header: {
+    fontSize: 40,
+    fontWeight: '500',
+    marginBottom: 50,
+    marginTop: -50,
+    color: 'white'
+  },
+  input: {
+    width: '65%',
+    height: '10%',
+    borderColor: 'white',
+    borderWidth: 2,
+    marginBottom: 15,
+    color: 'black',
+    backgroundColor: 'white',
+    borderRadius: 15
+  },
+  button: {
+    width: '50%',
+    height: '7%',
+    backgroundColor: 'gold',
+    borderRadius: 25
+    
+  },
+  text: {
+    color: 'black',
+    fontSize: 25,
+    fontWeight:'600',
+    textAlign: 'center',
+    marginTop: 8
   },
 });
